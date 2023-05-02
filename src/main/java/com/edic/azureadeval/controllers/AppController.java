10112001f75c524069e415c8afda3163a0907877
edcan-dev <israel.da.queenie@gmail.com>
2023-05-01 22:45:19 -0600

    segundo push eval - 01/05/23

--- src/main/java/com/edic/azureadeval/controllers/AppController.java
+++ src/main/java/com/edic/azureadeval/controllers/AppController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @Controller
+@CrossOrigin("*")
 public class AppController {
 
     @Autowired
